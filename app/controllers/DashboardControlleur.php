26b213976e94cba3eacdf86e5c5052a9353d5f24
Yrielle <yrielledarvisenet@gmail.com>
2026-02-16 21:45:20 +0300

    contr

--- app/controllers/DashboardControlleur.php
+++ app/controllers/DashboardControlleur.php
@@ -2,7 +2,6 @@
 
 namespace app\controllers;
 use app\model\Ville;
-use app\model\Besoin;
 use Flight;
 use flight\Engine;
 
@@ -17,24 +16,10 @@ class DashboardControlleur {
 
         $VilleModel = new Ville(Flight::db());
 
-        $villes = $VilleModel->getVillesAvecStats();
+        $villes = $VilleModel->getAllVilles();
 
         $this->app->render('dashboard', [
             'villes' => $villes
         ]);
     }
-
-    public function besoinsVille($idVille) {
-        $VilleModel = new Ville(Flight::db());
-        $BesoinModel = new Besoin(Flight::db());
-
-        $ville = $VilleModel->getVilleById($idVille);
-        $besoins = $BesoinModel->getBesoinsParVille($idVille);
-
-        $this->app->render('besoins/ville', [
-            'ville' => $ville,
-            'besoins' => $besoins
-        ]);
-    }
-
 }
