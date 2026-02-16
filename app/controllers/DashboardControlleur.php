636642d5742920d1687f8b66b323096512b12b40
RANDRIAMAHEFA Jean Pierre <jeanpierrerandriamahefa@gmail.com>
2026-02-16 15:19:46 +0300

    Modifcation du dashboard controlleur

--- app/controllers/DashboardControlleur.php
+++ app/controllers/DashboardControlleur.php
@@ -16,18 +16,10 @@ class DashboardControlleur {
     }
 
     public function dashboard() {
-        $besoins = Besoin::all();
-        $besoins_ville = BesoinVille::getBesoinVilleById();
         $villes = Ville::getAllVilles();
-        $distributions = Distribution::all();
-        $dons = Don::getAllDons();
 
         $this->app->render('Dashboard', [
-            'besoins' => $besoins,
-            'besoins_ville' => $besoins_ville,
-            'villes' => $villes,
-            'distributions' => $distributions,
-            'dons' => $dons
+            'villes' => $villes
         ]);
     }
 }
