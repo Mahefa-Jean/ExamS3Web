a875752a1e3ec1dd63815bac0bbd29471f52b4a3
RANDRIAMAHEFA Jean Pierre <jeanpierrerandriamahefa@gmail.com>
2026-02-16 23:04:05 +0300

    depart crud

--- app/views/menu.php
+++ app/views/menu.php
@@ -2,10 +2,9 @@
     <p class="site-menu__title">Navigation</p>
     <ul class="site-menu__list">
         <li><a href="/" class="site-menu__link <?= ($currentPage ?? '') === 'dashboard' ? 'active' : '' ?>">Dashboard</a></li>
-        <li><a href="/villes" class="site-menu__link <?= ($currentPage ?? '') === 'villes' ? 'active' : '' ?>">Gestion Villes</a></li>
-        <li><a href="/besoins" class="site-menu__link <?= ($currentPage ?? '') === 'besoins' ? 'active' : '' ?>">Gestion Besoins</a></li>
-        <li><a href="/dons" class="site-menu__link <?= ($currentPage ?? '') === 'dons' ? 'active' : '' ?>">Gestion Dons</a></li>
-        <li><a href="/distribution" class="site-menu__link <?= ($currentPage ?? '') === 'distribution' ? 'active' : '' ?>">Distribution</a></li>
-        <li><a href="/recapitulatif" class="site-menu__link <?= ($currentPage ?? '') === 'recapitulatif' ? 'active' : '' ?>">Récapitulatif</a></li>
+        <li><a href="/villes" class="site-menu__link <?= ($currentPage ?? '') === 'villes' ? 'active' : '' ?>">Villes</a></li>
+        <li><a href="/besoins" class="site-menu__link <?= ($currentPage ?? '') === 'besoins' ? 'active' : '' ?>">Besoins</a></li>
+        <li><a href="/dons" class="site-menu__link <?= ($currentPage ?? '') === 'dons' ? 'active' : '' ?>">Dons</a></li>
+        <li><a href="/distributions" class="site-menu__link <?= ($currentPage ?? '') === 'distribution' ? 'active' : '' ?>">Distribution</a></li>
     </ul>
 </nav>
